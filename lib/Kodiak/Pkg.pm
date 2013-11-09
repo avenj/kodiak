3b801f9cf5091c7c89723dc46069aec6c8b95d98
Jon Portnoy <avenj@cobaltirc.org>
2013-11-09 02:30:58 -0500

    Kodiak::Pkg; attribute outline
    	modified:   lib/Kodiak/Pkg.pm

--- lib/Kodiak/Pkg.pm
+++ lib/Kodiak/Pkg.pm
@@ -1,9 +1,84 @@
 package Kodiak::Pkg;
 use Kodiak::Base;
 
+# Identifiers:
 has [qw/
-  atom
+  category
+  name
+  version
 /];
+
+has slot => 0;
+
+has atom => sub {
+  my ($self) = @_;
+  for (qw/category name version slot/) {
+    confess "->atom called but missing attrib '$_'"
+      unless defined $self->$_
+  }
+  $self->_create_pkg_atom;
+};
+
+
+# Package meta:
+has [qw/
+  description
+  homepage
+  license
+/];
+
+has depends_hash => sub {
+  +{ build => [], runtime => [] }
+};
+
+has keywords    => sub { [] };
+has fetch_urls  => sub { [] };
+
+has settings    => sub { +{} };
+
+
+# Build phases:
+has [qw/
+  pl_init
+  pl_fetch
+  pl_unpack
+  pl_build
+  pl_test
+  pl_preinst
+  pl_install
+/];
+
+
+sub new {
+  my $class = shift;
+  my $self  = $class->SUPER::new(@_);
+
+  state $required = [ qw/
+    category
+    name
+    version
+  / ];
+
+  my @missing;
+  for (@$required) {
+    push @missing, $_ unless defined $self->$_
+  }
+  confess "Missing required parameter(s): ".join ', ', @missing
+    if @missing;
+
+  $self
+}
+
+sub _create_pkg_atom {
+  my ($self) = @_;
+  join '/', 
+    $self->category, 
+    $self->name, 
+    $self->version, 
+    $self->slot
+}
+
+
 
 # FIXME
 #  - Needs to contain all relevant pkg info
