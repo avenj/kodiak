8a96f17e8e6faffc70dfdf9568db101f2ffaabdc
Jon Portnoy <avenj@cobaltirc.org>
2013-11-10 05:24:04 -0500

    Kodiak POD outline
    	modified:   lib/Kodiak.pm

--- lib/Kodiak.pm
+++ lib/Kodiak.pm
@@ -4,4 +4,37 @@ use strict; use warnings;
 
 1;
 
+=pod
+
+=head1 NAME
+
+Kodiak - Kodiak software manager
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Flexible software manager.
+
+A work in progress.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+Licensed under the same terms as Perl.
+
+This dist includes code derived from or inspired by various CPAN projects
+licensed under the same or compatible terms, including:
+
+L<File::cd> by SYALTUT
+
+L<Mojolicious> by SRI et al
+
+L<Parallel::ForkManager> by DLUX, SZABDAB, et al
+
+L<YAML::Tiny> by ADAMK
+
+=cut
+
 # vim: ts=2 sw=2 et sts=2 ft=perl
