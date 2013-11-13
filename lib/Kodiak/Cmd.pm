4b02f865705a642bb4732b17ba6b309003242a46
Jon Portnoy <avenj@cobaltirc.org>
2013-11-13 10:48:28 -0500

    Cmd base class
    	modified:   lib/Kodiak/Cmd.pm

--- lib/Kodiak/Cmd.pm
+++ lib/Kodiak/Cmd.pm
@@ -5,4 +5,14 @@ use Kodiak::Base;
 
 has params => sub { [] };
 
+
+sub execute {
+  confess "'execute' method not implemented in ".(blessed $_[0] || $_[0])
+}
+
+sub undo {
+  confess "'undo' method not implemented in ".(blessed $_[0] || $_[0])
+}
+
+
 1;
