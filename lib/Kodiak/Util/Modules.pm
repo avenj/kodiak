d9f1a65feb624d27cece8b5d4a44b09310151ac9
Jon Portnoy <avenj@cobaltirc.org>
2013-11-15 13:08:46 -0500

    Util::Modules cleanups
    	modified:   lib/Kodiak/Util/Modules.pm

--- lib/Kodiak/Util/Modules.pm
+++ lib/Kodiak/Util/Modules.pm
@@ -6,36 +6,42 @@ use Exporter 'import';
 our @EXPORT_OK = qw/
   load_package
   load_or_return_error
+
   unload_package
-  package_to_filename
+
   package_is_loaded
+  package_to_filename
 /;
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
 
 sub _get_require_failure {
   my ($pkg) = @_;
   return if package_is_loaded($pkg);
 
-  my $file = package_to_filename($pkg);
+  my $file  = package_to_filename($pkg);
+  my $error = do {
+    local $@;
+    my $died = 'require returned false value' unless eval {; require "$file" };
+    $died = $@ if $@;
+    $died
+  };
 
-  local $@;
-  my $died = "require returned false"
-    unless eval {; require "$file" };
-  $died = $@ if $@;
-
-  unload_package($pkg) if $died;
-  $died
+  unload_package($pkg) if defined $error;
+  $error
 }
 
 sub load_or_return_error {
   my ($pkg) = @_;
   confess "Expected a package name" unless defined $pkg;
+
   _get_require_failure($pkg)
 }
 
 sub load_package {
   my ($pkg) = @_;
   confess "Expected a package name" unless defined $pkg;
+
   my $died;
   return $pkg unless $died = _get_require_failure($pkg);
   confess "Failed to load '$pkg': $died"
@@ -44,14 +50,19 @@ sub load_package {
 sub unload_package {
   my ($pkg) = @_;
   my $file  = package_to_filename($pkg);
+
   no strict 'refs';
+
   @{ $pkg .'::ISA' } = ();
+
   my $table = $pkg . '::';
   for (keys %$table) {
     next if substr($_, -2, 2) eq '::';
     delete $table->{$_}
   }
+
   delete $INC{$file};
+
   1
 }
 
@@ -59,7 +70,7 @@ sub package_is_loaded {
   my ($pkg) = @_;
 
   no strict 'refs';
-  
+
   return 1
     if defined ${ $pkg .'::VERSION' }
     or @{ $pkg .'::ISA' };
@@ -77,6 +88,7 @@ sub package_is_loaded {
 sub package_to_filename {
   my ($pkg) = @_;
   confess "Expected a package name" unless defined $pkg;
+
   join('/', split /(?:\'|::)/, $pkg) . '.pm'
 }
 
